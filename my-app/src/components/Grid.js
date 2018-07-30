6396b86ff52ce1f1a6fb7759db680d99a2f4da01
Megan Landau <landaumd@g.cofc.edu>
2018-07-29 21:42:11 -0700

    jumbotron, carousel, and cards working

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -3,19 +3,58 @@ import React, { Component } from "react";
 import './Grid.css';
 import MyCard from './MyCard.js';
 import MyJumbo from './MyJumbo.js';
-
+import Collapsible from './Collapsible.js';
 import StackGrid, { transitions } from "react-stack-grid";
+import MyCarousel from './MyCarousel.js';
 
 const { scaleDown } = transitions;
+
+// const items = [];
+
+let items = [{title:'hello'}, {title:'world'}];
+
+// const pics = [{
+//     one: {id: 1, type: MyCard}
+// }]
+
+// for (let i = 0; i < 30; i += 1) {
+//     const id = Math.random().toString(36).substr(2, 9);
+//     const height = Math.floor((Math.random() * (300 - 80)) + 80);
+//
+//     items.push({
+//         id,
+//         height,
+//         active: false,
+//         actualHeight: height
+//     });
+// }
 
 // https://tsuyoshiwada.github.io/react-stack-grid/#/
 // https://github.com/tsuyoshiwada/react-stack-grid#live-demo
 
 class Grid extends Component {
+    state = { items };
+
+    // changeItemSize = (id) => {
+    //     this.setState({
+    //         items: this.state.items.map(o => (
+    //             o.id !== id ? o : {
+    //                 ...o,
+    //                 active: !o.active,
+    //                 height: !o.active ? o.height * 1.5 : o.actualHeight,
+    //             }
+    //         )),
+    //     }, () => {
+    //         this.grid.updateLayout();
+    //     });
+    //     console.log("grrrr");
+    // };
+
     render() {
         return (
             <div className="Grid-top">
                 <StackGrid
+                    gridRef={grid => this.grid = grid}
                     // style={{'background-color': 'red'}}
                     // style={{'padding-top': '20rem}}
                     columnWidth={300}
@@ -26,20 +65,43 @@ class Grid extends Component {
                     // horizontal={true}
                     // rtl={true}
                 >
-                    <MyCard />
-                    <MyCard />
-                    <MyCard />
-                    <MyJumbo />
-                    <MyJumbo />
-                    <MyCard />
-                    <MyCard />
-                    <MyCard />
-                    <MyCard />
-                    {/*<div>*/}
-                    {/*<div key="key1">Item 1</div>*/}
-                    {/*<div key="key2">Item 2</div>*/}
-                    {/*<div key="key3">Item 3</div>*/}
-                    {/*</div>*/}
+
+                    {/*{this.state.items.map(({ id, active, height }) => (*/}
+                    {/*{this.state.items.map(({item}) => (*/}
+                        {/*<span*/}
+                            {/*{*/}
+                                {/*React.cloneElement(*/}
+                                    {/*MyCard,*/}
+                                    {/*{title: item.title}*/}
+                                {/*)*/}
+                            {/*}*/}
+                            {/*// key={id}*/}
+                            {/*// style={{*/}
+                            {/*//     transition: 'background 200ms ease-out',*/}
+                            {/*//     background: active ? '#c9cae3' : '#dfe0df',*/}
+                            {/*//     height,*/}
+                            {/*// }}*/}
+                            {/*// onClick={() => this.changeItemSize(id)}*/}
+                        {/*/>*/}
+                    {/*))}*/}
+
+                        < MyCard/>
+                        < MyCard />
+                        < MyCarousel/>
+                        < MyCard />
+                        < MyJumbo />
+                        < MyJumbo />
+                        < MyCard />
+                        {/*< Collapsible key={1} onClick={() => this.changeItemSize(0)}/>*/}
+                        < MyCard />
+                        < MyCard />
+
+                    {/*<div>*/
+                    /*<div key="key1">Item 1</div>*/
+                    /*<div key="key2">Item 2</div>*/
+                    /*<div key="key3">Item 3</div>*/
+                    /*</div>*/}
+
                 </StackGrid>
             </div>
         );
