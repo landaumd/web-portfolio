68f2ec7b6d8948b4dfa5b261712e42bdda68ae12
Megan Landau <landaumd@g.cofc.edu>
2018-08-04 22:38:21 -0700

    added redux, needs cleanup

---
+++ redux-cra/src/components/Grid.js
@@ -0,0 +1,107 @@
+import test from './data.json';
+import React, { Component } from "react";
+import './Grid.css';
+import MyCard from './MyCard.js';
+import MyJumbo from './MyJumbo.js';
+import StackGrid, { transitions } from "react-stack-grid";
+import MyCarousel from './MyCarousel.js';
+import sizeMe from 'react-sizeme';
+const { scaleDown } = transitions;
+
+// https://tsuyoshiwada.github.io/react-stack-grid/#/
+// https://github.com/tsuyoshiwada/react-stack-grid#live-demo
+
+class Grid extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidUpdate = () => {
+        this.grid.updateLayout();
+    }
+
+    // getNumbers() {
+    //     if (this.elem) {
+    //         return Object.keys(this.elem).map( (key) => {
+    //             return <p>{key} => {this.elem[key]}</p>;
+    //         });
+    //     } else {
+    //         return <p>data is not available</p>;
+    //     }
+    // }
+
+    render() {
+
+        const {
+            size: {
+                width
+            }
+        } = this.props;
+
+        return (
+            <div className="Grid-container">
+                <StackGrid
+                    // ref={this.grid}
+                    gridRef={grid => this.grid = grid}
+                    //style={{'background-color': 'red'}}
+                    // style={{'padding-top': '20rem}}
+                    // columnWidth={300}
+                    gutterWidth={15}
+                    columnWidth={width <= 400 ? '100%' : (width <= 650 ? '50%' : (width <= 950 ? '33%' : '25%'))}
+                    gutterHeight={15}
+                    enter={scaleDown.enter}
+                    monitorImagesLoaded={true}
+                    // horizontal={true}
+                    // rtl={true}
+
+                >
+
+                    {/*{this.state.items.map(({ id, active, height }) => (*/}
+                    {/*{this.state.items.map(({item}) => (*/}
+                        {/*<span*/}
+                            {/*{*/}
+                                {/*React.cloneElement(*/}
+                                    {/*MyCard,*/}
+                                    {/*{title: item.title}*/}
+                                {/*)*/}
+                            {/*}*/}
+                            {/*// key={id}*/}
+                            {/*// style={{*/}
+                            {/*//     transition: 'background 200ms ease-out',*/}
+                            {/*//     background: active ? '#c9cae3' : '#dfe0df',*/}
+                            {/*//     height,*/}
+                            {/*// }}*/}
+                            {/*// onClick={() => this.changeItemSize(id)}*/}
+                        {/*/>*/}
+                    {/*))}*/}
+
+                    < MyCard/>
+                        < MyCard />
+                        < MyCarousel/>
+                        < MyCard />
+                        < MyJumbo />
+                        < MyJumbo />
+                        < MyCarousel/>
+                        < MyCard />
+                        {/*< Collapsible key={1} onClick={() => this.changeItemSize(0)}/>*/}
+                        < MyCard />
+                        < MyCard />
+                        < MyCarousel/>
+                    {/*<ul>*/}
+                        {/*{Object.keys(this.elem).map((v, i) => <li key={i}> {v} {this.elem[v]} </li> )}*/}
+                    {/*</ul>*/}
+
+                    {/*<div>*/
+                    /*<div key="key1">Item 1</div>*/
+                    /*<div key="key2">Item 2</div>*/
+                    /*<div key="key3">Item 3</div>*/
+                    /*</div>*/}
+
+                </StackGrid>
+            </div>
+        );
+    }
+}
+
+export default sizeMe()(Grid);
