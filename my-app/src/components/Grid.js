b40ba2c86c787f9a7ddf5671da54c6fbfc74c95a
Phillip Byrd <Phill.e.byrd@gmail.com>
2018-08-06 19:07:26 -0700

    working on the JSON Card Problem

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -1,30 +1,90 @@
-import test from './data.json';
+// import test from './data.json';
+import cardTest from './CardTest.json';
 import React, { Component } from "react";
 import './Grid.css';
 import MyCard from './MyCard.js';
 import MyJumbo from './MyJumbo.js';
 import StackGrid, { transitions } from "react-stack-grid";
+import easyTest from './CardTest';
+import simpleTest from '../simpleTest';
 import MyCarousel from './MyCarousel.js';
 import sizeMe from 'react-sizeme';
 const { scaleDown } = transitions;
 
+
+
+
 // https://tsuyoshiwada.github.io/react-stack-grid/#/
 // https://github.com/tsuyoshiwada/react-stack-grid#live-demo
+
+
 
 class Grid extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data : cardTest.cards,
+            type : "MyCard",
+            title: "This is the title of my Card 1",
+            subtitle: "here is some other small info",
+            bodyText: "Here is a whole bunch of test body text",
+            image : "Here is a URL to an image"
 
-        // this.state = {
+
+
+        }
+
+
         //
-        // };
-        this.elem = test;
+        // this.arr = this.state.data;
+        // // this.arr.push(Object.keys(this.state.data));
+        // console.log(this.arr["Card1"].["title"])
+        // console.log(typeof(this.arr));
+        //
+        // for(var i=0;i<this.arr.size;i++ ){
+        //     console.log(this.arr[i]);
 
-        this.arr = [];
-        Object.keys(this.elem).forEach(function(key) {
-            this.arr.push(test[key]);
-        });
+            // if (this.arr[i].length > 0){
+            //     for(var j=0;j<this.arr[i].length;j++ ) {
+            //         console.log(this.arr[i][j]["body-text"]);
+            //     }
+            // }
+        // }
+        //
+        // console.log("end");
+        // Object.keys(this.elem).forEach(function(key) {
+        //     console.log(Object.keys(this.elem));
 
+        // });
+
+
+        // console.log(Object.keys(this.state.data));
+        // console.log(this.state.data);
+        // console.log(this.state.data);
+        // var keys = Object.keys(this.state.data);
+        // console.log(keys);
+
+
+
+        // console.log(test[0].title);
+        // console.log(test[0].thumbnail);
+        // console.log(test[0].description);
+        // console.log(test[0].content.text);
+        // console.log(keys);
+        //
+        // var myJson = JSON.stringify(test);
+        // console.log(myJson);
+        //
+        //
+        // var allKeys = keys.map((t) => (<div>{test[t].title}</div>));
+        // console.log(allKeys);
+        //
+        // console.log(allKeys[0].props.children);
+        // console.log(allKeys[0].props.children.data);
+        // console.log(allKeys[1].props.children);
+        //
+        // console.log(Object.keys(test).map((v, i) => <li key={i}> {v} {test[v]} </li> ));
+        // return (<div>{allKeys}</div>)
     }
 
     componentDidUpdate = () => {
@@ -32,7 +92,7 @@ class Grid extends Component {
     }
 
     // getNumbers() {
-    //     if (this.elem) {
+    //     if (this.arr) {
     //         return Object.keys(this.elem).map( (key) => {
     //             return <p>{key} => {this.elem[key]}</p>;
     //         });
@@ -40,6 +100,9 @@ class Grid extends Component {
     //         return <p>data is not available</p>;
     //     }
     // }
+
+
+
 
     render() {
 
@@ -86,34 +149,29 @@ class Grid extends Component {
                         {/*/>*/}
                     {/*))}*/}
 
-                    <ul>{this.arr.map(item => <MyCard key={item.label} label={item.label} value={item.value} />)}</ul>;
+                    {/*<ul>{this.arr.map(item => <MyCard key={item.label} label={item.label} value={item.value} />)}</ul>;*/}
 
-                    {/*{Object.keys(this.elem).map((v, i) => <li key={i}> {v} {test[v]} </li> )}*/}
+
 
                     {/*{ this.elem.map((item, index) => { return <MyCard {... item}/> }) }*/}
 
 
-                    {/*< MyCard/>*/}
-                        {/*< MyCard />*/}
-                        {/*< MyCarousel/>*/}
-                        {/*< MyCard />*/}
-                        {/*< MyJumbo />*/}
-                        {/*< MyJumbo />*/}
-                        {/*< MyCarousel/>*/}
-                        {/*< MyCard />*/}
-                        {/*/!*< Collapsible key={1} onClick={() => this.changeItemSize(0)}/>*!/*/}
-                        {/*< MyCard />*/}
-                        {/*< MyCard />*/}
-                        {/*< MyCarousel/>*/}
-                    {/*<ul>*/}
-                        {/*{Object.keys(this.elem).map((v, i) => <li key={i}> {v} {this.elem[v]} </li> )}*/}
-                    {/*</ul>*/}
 
-                    {/*<div>*/
-                    /*<div key="key1">Item 1</div>*/
-                    /*<div key="key2">Item 2</div>*/
-                    /*<div key="key3">Item 3</div>*/
-                    /*</div>*/}
+                    < MyCard title={this.state.title} subtitle={this.state.subtitle} bodyText={this.state.bodyText}/>
+                        {/*< MyCard />*/}
+                        {/*< MyCarousel/>*/}
+                        {/*< MyCard />*/}
+                        {/*< MyJumbo />*/}
+                        {/*< MyJumbo />*/}
+                        {/*< MyCarousel/>*/}
+                        {/*< MyCard />*/}
+                        {/*< MyCard />*/}
+                        {/*< MyCard />*/}
+                        {/*< MyCarousel/>*/}
+
+
+
+
 
                 </StackGrid>
             </div>
