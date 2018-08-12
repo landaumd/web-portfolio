326935edce22875afe2b71d0e346e1d4164c3f1f
Megan Landau <landaumd@g.cofc.edu>
2018-08-12 13:55:31 -0700

    Cleaned up a little (#20)

--- redux-cra/src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import simpleReducer from './simpleReducer.js';
-import counter from './counter.js';
-
-export default combineReducers({
-    counter,
-    simpleReducer,
-});
