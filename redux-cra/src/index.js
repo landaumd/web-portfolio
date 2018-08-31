c74797e2fd17e0bdea9cbbc595f21753fd29b962
Phillip Byrd <Phill.e.byrd@gmail.com>
2018-08-31 12:11:03 -0700

    redoing the google analytics through NPM modules

--- redux-cra/src/index.js
+++ redux-cra/src/index.js
@@ -6,10 +6,13 @@ import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReactGA from 'react-ga';
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
+    ReactGA.initialize('UA-125039397-1'),
     document.getElementById('root')
 );
 registerServiceWorker();
